7a178a9610a712b6eb84948457229f01bff3eeb9
Prafull37 <prafullsingh37@gmail.com>
2022-02-24 01:18:25 +0530

    Input and TextError is ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import FormContainer from './components/FormContainer';
+import Input from "./components/Input";
+
 function App() {
   return (
     <div className="App">
@@ -8,8 +10,21 @@ function App() {
       name="journey">
         {
           (formik) =>{
-            console.log(formik)
-            return (<div>Hello World</div>)
+            console.log("formik",formik)
+            return (<Input 
+              id={"source"}
+              label="Source Location"
+              onInputChange={formik.handleChange}
+              onInputBlur={(e)=>{console.log(e);formik.handleBlur(e)}}
+              value={formik.values.journey.source}
+              name="journey.source"
+              validate={(values)=>{
+                if(!values){
+                  return "source cannot be empty";
+                }
+            }}
+            required={true}
+            / >)
           }
         }
       </FormContainer>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,45 @@
-import { Field,ErrorMessage } from "formik";
-
+import { Field } from "formik";
+import TextError from "./TextError";
+import "../css/components/input.css";
 
 export default function Input(props){
 
+    const {name,type,id,validate,onInputChange,onInputBlur,value,classes,style,required} = props;
+    const inputClass = !!classes ? classes:"";
+
     return (
-        <div className="input-wrapper">
-            <label htmlFor={props.id}>{props.label}</label>
-            <Field />
-            <ErrorMessage name={props.name} />
+        <div className={`input-wrapper ${props.wrapperClasses}`}>
+            <label htmlFor={props.id}
+            className={`label ${props.labelClasses}`}>{props.label}{required?<span className="star">*</span>:""}</label>
+            <Field 
+                name={name}
+                type={type}
+                id={id}
+                className ={`input ${inputClass}`}
+                validate={validate}
+                onChange={onInputChange}
+                onBlur={onInputBlur}
+                value={value}
+                style={style}
+            />
+            <TextError name={props.name}/>
         </div>
     )
 }
+
+/**
+ * <Input 
+              id={"source"}
+              label="Source Location"
+              onInputChange={formik.handleChange}
+              onInputBlur={(e)=>{console.log(e);formik.handleBlur(e)}}
+              value={formik.values.journey.source}
+              name="journey.source"
+              validate={(values)=>{
+                if(!values){
+                  return "source cannot be empty";
+                }
+            }}
+            required={true}
+            / >
+ */
